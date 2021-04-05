4083357e7d48f37592581ed494d028e5448cceec
s8mdisla <s8mdisla@stud.uni-saarland.de>
2021-04-05 22:48:50 +0200

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Particles from 'react-particles-js';
 import Navbar from './component/Navbar'
+import Header from './component/Header'
 
 function App() {
   return (
-    <Navbar/>
+    <div>
+      <Particles
+        className="paricles-canvas"
+        params={{
+          particles: {
+            number: {
+              value:30,
+              density: {
+                enable: true,
+                value_area:900
+              }
+            },
+            shape: {
+              type: "star",
+              stroke: {
+                width: 6,
+                color:"#f9ab00"
+              }
+            }
+          }
+        }}
+      />
+      <Navbar />
+      <Header/>
+    </div>
   )
 }
 
